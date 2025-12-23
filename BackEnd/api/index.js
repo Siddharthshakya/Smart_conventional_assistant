498d01b506e10386ef570b87650034f6bc11b1fb
siddharth shakya <siddharthshakya2914@gmail.com>
2025-12-23 12:57:23 +0530

    Fix import path for chatRoutes in index.js

--- BackEnd/api/index.js
+++ BackEnd/api/index.js
@@ -2,10 +2,9 @@ import express from "express";
 import "dotenv/config";
 import cors from "cors";
 import mongoose from "mongoose";
-import chatRoutes from "./routes/chat.js";
+import chatRoutes from "../routes/chat.js";
 
 const app = express();
-const PORT = 8080;
 
 app.use(express.json());
 app.use(cors());
