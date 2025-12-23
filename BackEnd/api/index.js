feeb97e7d15e2cf279427cc5ccb74c5005e40020
siddharth shakya <siddharthshakya2914@gmail.com>
2025-12-23 12:54:24 +0530

    Initialize Express server with MongoDB and chat routes
    Set up Express server with MongoDB connection and chat routes.

--- BackEnd/api/index.js
+++ BackEnd/api/index.js
@@ -1,1 +1,30 @@
+import express from "express";
+import "dotenv/config";
+import cors from "cors";
+import mongoose from "mongoose";
+import chatRoutes from "./routes/chat.js";
 
+const app = express();
+const PORT = 8080;
+
+app.use(express.json());
+app.use(cors());
+
+app.use("/chat", chatRoutes);
+
+
+// MongoDb Connection
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log("Connected to Database successfully!");
+  } catch (err) {
+    console.error("MongoDB connection error:", err);
+  }
+};
+
+connectDB();
+
+/* Export app for Vercel */
+export default app;
